e5b75eb4f94e35cc9f9605ce1d79a047f40cc8b1
Volodymyr <vthehang@gmail.com>
2022-09-22 10:12:50 +0200

    added second task

---
+++ lab01/task02.py
@@ -0,0 +1,12 @@
+import argparse
+import operator
+
+parser = argparse.ArgumentParser()
+parser.add_argument("func", type=str)
+parser.add_argument("num1", type=float)
+parser.add_argument("num2", type=float)
+args = parser.parse_args()
+
+func = getattr(operator, args.func, None)
+
+print(func(args.num1, args.num2)) if func else print("Func name is not correct")
