74a6c3150aba9137509c1c7efa476ee4244fc363
Volodymyr <vthehang@gmail.com>
2022-09-22 12:55:19 +0200

    modified 3rd task, now it looks pretty :)

--- lab01/task03.py
+++ lab01/task03.py
@@ -1,16 +1,22 @@
 import argparse
 
-parser = argparse.ArgumentParser()
-parser.add_argument("expression", type=str)
-args = parser.parse_args()
 
-symbols = "0123456789+-"
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("expression", type=str)
+    args = parser.parse_args()
 
-for index, char in enumerate(args.expression):
-    if (char not in symbols) or ((char == "+" or char == "-") and (args.expression[index - 1] == char)):
-        result = (False, None)
-        print(result)
-        exit(1)
+    result = proccessing(args.expression)
+    print(result)
 
-result = (True, eval(args.expression))
-print(result)
+
+def proccessing(expression: str):
+    symbols = "0123456789+-"
+    for index, char in enumerate(expression):
+        if (char not in symbols) or ((char == "+" or char == "-") and (expression[index - 1] == char)):
+            return (False, None)
+    return (True, eval(expression))
+
+
+if __name__ == "__main__":
+    main()
